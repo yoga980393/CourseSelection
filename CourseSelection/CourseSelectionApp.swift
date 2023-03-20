723bca3279332fb9eeecfc593632731aff2e174a
yoga980393 <wggisdde@gmail.com>
2023-03-20 21:32:57 +0800

    Initial Commit

---
+++ CourseSelection/CourseSelectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseSelectionApp.swift
+//  CourseSelection
+//
+//  Created by 張祐嘉 on 2023/3/20.
+//
+
+import SwiftUI
+
+@main
+struct CourseSelectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
